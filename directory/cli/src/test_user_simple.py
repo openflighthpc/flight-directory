e268d993b76c0951a6778582e317a926c10f7468
Andrew <andrew.toghill@alces-software.com>
2018-11-28 15:42:52 +0000

    Test simple user modify with/without modification

--- directory/cli/src/test_user_simple.py
+++ directory/cli/src/test_user_simple.py
@@ -58,7 +58,7 @@ def test_user_create_without_uid_calls_ipa_correctly(mocker):
         'walterwhite\nWalter\nWhite\nheisenberg@example.com\nNo\n',
     )
 
-def test_user_modify_calls_ipa_correctly(mocker):
+def test_user_modify_with_modifications_calls_ipa_correctly(mocker):
     test_utils.mock_ipa_find_output(mocker)
     test_utils.mock_options_passed_to_ipa(
         mocker,
@@ -79,5 +79,30 @@ def test_user_modify_calls_ipa_correctly(mocker):
             )
         ],
         'walterwhite\nWalt\nJackson\nnotheisenberg@example.com\nNo\n',
+    )
+
+# We would expect this test to match the defaults mocked within
+# mock_ipa_find_output
+def test_user_modify_without_modifications_calls_ipa_correctly(mocker):
+    test_utils.mock_ipa_find_output(mocker)
+    test_utils.mock_options_passed_to_ipa(
+        mocker,
+        ['user', 'modify'],
+        [
+            (
+                'user-mod',
+                [
+                    [
+                        'walterwhite',
+                        '--cn', 'walterwhite_first walterwhite_last',
+                        '--displayname', 'walterwhite_first walterwhite_last',
+                        '--email', 'walterwhite_email',
+                        '--first', 'walterwhite_first',
+                        '--last', 'walterwhite_last'
+                    ]
+                ]
+            )
+        ],
+        'walterwhite\n\n\n\n\n'
     )
 
